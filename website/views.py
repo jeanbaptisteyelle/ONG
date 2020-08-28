7ce34545a7861c51f93285846df746c121fe1e8f
jeanbaptisteyelle <yellejeanbaptiste@gmail.com>
2020-08-28 13:12:04 +0000

    first commit

---
+++ website/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    datas={
+
+    }
+    return render(request,'pages/website/index.html')
+
+def about(request):
+    datas={
+
+    }
+    return render(request,'pages/website/about.html')
+
+def volunteers(request):
+    datas={
+
+    }
+    return render(request,'pages/website/volunteers.html')
+
+def faq(request):
+    datas={
+
+    }
+    return render(request,'pages/website/faq.html')
+
+def donate(request):
+    datas={
+
+    }
+    return render(request,'pages/website/donate.html')
+
+def causes(request):
+    datas={
+
+    }
+    return render(request,'pages/website/causes.html')
+
+def cause_single(request):
+    datas={
+
+    }
+    return render(request,'pages/website/cause-single.html')
+
+def events(request):
+    datas={
+
+    }
+    return render(request,'pages/website/events.html')
+
+def event_single(request):
+    datas={
+
+    }
+    return render(request,'pages/website/event-single.html')
+
+def blog(request):
+    datas={
+
+    }
+    return render(request,'pages/website/blog.html')
+
+def blog_single(request):
+    datas={
+
+    }
+    return render(request,'pages/website/blog-single.html')
+
+def contact(request):
+    datas={
+
+    }
+    return render(request,'pages/website/contact.html')
+
